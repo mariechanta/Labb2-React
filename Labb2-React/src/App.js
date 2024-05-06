4749c36004ea58cae812c7a6b4984813ea560cee
mariechanta <52567733+mariechanta@users.noreply.github.com>
2024-05-06 11:20:30 +0200

    Fist commit

---
+++ Labb2-React/src/App.js
@@ -0,0 +1,12 @@
+import react from 'react';
+
+const App = () => {
+    return (
+        <App>
+            <h1>Welcome to school APP</h1>
+            <img src="skolans_logo.png" alt="Skolans logo" />
+        </App>
+    );
+};
+
+export default App;
