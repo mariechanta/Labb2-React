31af31224d911557a9c6252baa18a814a6e9e70f
mariechanta <52567733+mariechanta@users.noreply.github.com>
2024-05-11 14:56:18 +0200

    Commit

--- vite-project/src/Header.jsx
+++ vite-project/src/Header.jsx
@@ -1,24 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-import styled from 'styled-components';
 
-const StyledHeader = styled.header`
-  background-color: #333;
-  color: #fff;
-  padding: 20px;
-`;
 
 const Header = () => {
   return (
-    <StyledHeader>
-      <h1>Welcome to school APP</h1>
-            <img
-                src="img1.png"
-                alt="Beskrivande text för bilden"
-                style={{ width: '1000px', height: '600px' }}
-            />
-
-    </StyledHeader>
+      <><h1>Welcome to school APP</h1><img
+          src="img1.png"
+          alt="Beskrivande text för bilden"
+          style={{ width: '600px', height: '400px' }} /></>
   );
 };
 
